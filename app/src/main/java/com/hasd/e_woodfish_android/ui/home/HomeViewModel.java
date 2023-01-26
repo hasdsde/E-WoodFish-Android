20b3ffb9a291ac1ab2a738d51485e7b1ece15db9
hasdsd <1259305706@qq.com>
2023-01-26 22:09:49 +0800

    第一次提交

---
+++ app/src/main/java/com/hasd/e_woodfish_android/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.hasd.e_woodfish_android.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("这里是Home首页");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
