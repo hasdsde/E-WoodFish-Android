b52313e8377ab20037fb42202f58bed163bab042
hasdsd <1259305706@qq.com>
2023-01-27 16:33:07 +0800

    尝试修改主题

---
+++ app/src/main/java/com/hasd/e_woodfish_android/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.hasd.e_woodfish_android.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hasd.e_woodfish_android.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
