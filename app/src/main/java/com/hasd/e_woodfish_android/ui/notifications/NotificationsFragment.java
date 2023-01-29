1c29d9d55650c0fa9e7e63c59b7633ab30d1fbcc
hasdsd <1259305706@qq.com>
2023-01-29 16:45:17 +0800

    我县备份一下

--- app/src/main/java/com/hasd/e_woodfish_android/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/hasd/e_woodfish_android/ui/notifications/NotificationsFragment.java
@@ -16,8 +16,14 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.VolleyError;
 import com.hasd.e_woodfish_android.R;
 import com.hasd.e_woodfish_android.databinding.FragmentNotificationsBinding;
+import com.hasd.e_woodfish_android.utils.Api;
+
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 
 public class NotificationsFragment extends Fragment {
     private TextView userInfo;
@@ -35,13 +41,30 @@ public class NotificationsFragment extends Fragment {
         Refresh = binding.btnRefresh;
         table = binding.tlInfo;
 
+        Api.sendGetRequest("https://muyu.hasdsd.cn/api/user/top", getActivity(), new Api.VolleyCallback() {
+            @Override
+            public void onSuccess(String result) {
+                JSONObject res = JSONUtil.parseObj(result);
+                if (res.getStr("code").toString().equals("200")) {
+                    JSONArray data = res.getJSONArray("data");
+                    for (int i = 0; i < data.size(); i++) {
+                        JSONObject item = data.getJSONObject(i);
+                        
+                    }
+                }
+            }
 
+            @Override
+            public void onError(VolleyError error) {
+
+            }
+        });
         tableRow = new TableRow(getActivity());
         tableRow.setGravity(Gravity.CENTER);
         TextView textView1 = new TextView(getActivity());
         TextView textView2 = new TextView(getActivity());
         TextView textView3 = new TextView(getActivity());
-        
+
         textView1.setText("1");
         textView2.setText("由Java添加的代码");
         textView3.setText("100");
